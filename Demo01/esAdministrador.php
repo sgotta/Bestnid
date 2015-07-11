7ca20b1afa603d38ace18ba5f4e60d1daa15e1db
CaritoRomano <romano.carolina90@gmail.com>
2015-07-11 15:45:56 -0300

    esAdministrador

--- Demo01/esAdministrador.php
+++ Demo01/esAdministrador.php
@@ -3,12 +3,12 @@
 		include("conexion.php");
 		$con=mysql_connect($host,$user,$pw) or die ("problemas al conectar");
 		mysql_select_db($db,$con) or die ("problemas al conectarDB");
-		//CANTIDAD DE notificaciones PARA BADGE
 		$usuario=mysql_query("SELECT * 
 					 FROM usuario 
-					WHERE usuario.nombre_usuario='$_SESSION[username]'") or die ("problemas en consulta:".mysql_error());
-		
-		if($usuario['administrador'] == 0){
+					WHERE nombre_usuario='$_SESSION[username]'") or die ("problemas en consulta:".mysql_error());
+
+		$usuario= mysql_fetch_array($usuario);
+		if($usuario['admin'] == 1){
 			echo '<li><a href="paneldecontrol.php">Panel de control</a></li>';
 		}
  
